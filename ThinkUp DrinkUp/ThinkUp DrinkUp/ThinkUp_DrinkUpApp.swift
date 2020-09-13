db24e8cd02b73d49adb60af30588e8e3dffb7d34
adam mcbride <adam.mcbride@gmail.com>
2020-09-12 23:20:56 -0400

    added new xcode project

---
+++ ThinkUp DrinkUp/ThinkUp DrinkUp/ThinkUp_DrinkUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThinkUp_DrinkUpApp.swift
+//  ThinkUp DrinkUp
+//
+//  Created by Adam McBride on 9/12/20.
+//
+
+import SwiftUI
+
+@main
+struct ThinkUp_DrinkUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
